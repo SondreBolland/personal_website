c6a1677039a6e292a8715788f4554cd5cd49a170
Sondre Bolland <sondre.bolland@uib.no>
2025-07-07 00:03:26 +0200

    test

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,8 +4,9 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./static/frontend"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+    publicPath: '/',
   },
   module: {
     rules: [
--- personal_website/urls.py
+++ personal_website/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('djago/backend/', include('backend.urls')),
+    path('django/backend/', include('backend.urls')),
     path('', include('frontend.urls'))
 ]
