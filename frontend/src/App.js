40bd5d731cc30d42913842337663f6bceaa660c4
Sondre Bolland <sondre.bolland@uib.no>
2025-07-08 19:23:18 +0200

    Added url patterns for each page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createRoot } from "react-dom/client";
+    import React from "react";
+    import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+    import { createRoot } from "react-dom/client";
 
-import TopPane from "./components/TopPane";
-import Footer from "./components/Footer";
+    import TopPane from "./components/TopPane";
+    import Footer from "./components/Footer";
 
-import HomePage from "./pages/HomePage";
-import Publications from "./pages/Publications";
-import Learning from "./pages/Learning";
-import Projects from "./pages/Projects";
-import Message from "./pages/Message";
+    import HomePage from "./pages/HomePage";
+    import Publications from "./pages/Publications";
+    import Learning from "./pages/Learning";
+    import Projects from "./pages/Projects";
+    import Message from "./pages/Message";
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <TopPane />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/publications" element={<Publications />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/learning" element={<Learning />} />
-            <Route path="/message" element={<Message />} />
-          </Routes>
-          <Footer />
-        </main>
-      </div>
-    </Router>
-  );
-}
+    export default function App() {
+    return (
+        <Router>
+        <div>
+            <TopPane />
+            <main>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/publications" element={<Publications />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/learning" element={<Learning />} />
+                <Route path="/message" element={<Message />} />
+            </Routes>
+            <Footer />
+            </main>
+        </div>
+        </Router>
+    );
+    }
 
-const appDiv = document.getElementById("app");
-const root = createRoot(appDiv);
-root.render(<App />);
+    const appDiv = document.getElementById("app");
+    const root = createRoot(appDiv);
+    root.render(<App />);
--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,8 @@ app_name = 'frontend'
 
 urlpatterns = [
     path('', index, name=''),
+    path('publications', index),
+    path('projects', index),
+    path('learning', index),
     path('message', index),
 ]
